e5ccfc9bcb5883faa8ef9c0555a5aabe341a7ef4
daniiladamov <daniiladamov@mail.ru>
2022-08-03 18:50:29 +0300

    exam init

---
+++ src/main/java/com/example/liga_exam/LigaExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.liga_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LigaExamApplication.class, args);
+    }
+
+}
